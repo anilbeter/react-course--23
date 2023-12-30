73f29dde603e3edf608674ff1828325c4996d922
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-12-30 21:44:43 +0300

    Array sort method

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -249,3 +249,11 @@ adventureBooks;
 
 const pagesAllBooks = books.reduce((sum, book) => sum + book.pages, 0);
 pagesAllBooks;
+
+const arr = [3, 7, 1, 9, 6];
+const sorted = arr.slice().sort((a, b) => a - b);
+sorted; // [ 1, 3, 6, 7, 9 ]
+arr; // [ 3, 7, 1, 9, 6 ]
+
+const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
+sortedByPages;
