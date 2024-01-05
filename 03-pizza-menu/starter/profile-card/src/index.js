6719d41bfc09ad31987974545d22df77db01ed5e
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-05 21:09:59 +0300

    add profile-card challenge

---
+++ 03-pizza-menu/starter/profile-card/src/index.js
@@ -0,0 +1,62 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="wp.jpg" alt="anil" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Anil Beter</h1>
+      <p style={{ fontWeight: "450" }}>
+        Game ve web development alanlarinda calisiyorum.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="teal" />
+      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="Unreal Engine" emoji="👶" color="orangered" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
