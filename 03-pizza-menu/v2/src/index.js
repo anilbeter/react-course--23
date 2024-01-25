645dd1b9b5f8588432f1a45fa2186405dd3354fd
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-25 19:55:51 +0300

    create more components

--- 03-pizza-menu/v2/src/index.js
+++ 03-pizza-menu/v2/src/index.js
@@ -49,11 +49,30 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
-      <Pizza />
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
       <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
   );
 }
 
