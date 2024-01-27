800ec2883d3554f9c8015fa4a2070abe09c6f4dd
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-27 20:49:28 +0300

    create profile card exercise starter files

---
+++ 03-pizza-menu/v2/profile-card-exercise/src/App.js
@@ -0,0 +1,24 @@
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Intro() {}
+
+function SkillList() {}
+
+function Avatar() {
+  return <div className="avatar"></div>;
+}
