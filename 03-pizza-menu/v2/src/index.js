47b4fdfeaaa4e4edfd86547729e0082c6268911b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-28 10:16:06 +0300

    conditional rendering with ternary opr

--- 03-pizza-menu/v2/src/index.js
+++ 03-pizza-menu/v2/src/index.js
@@ -74,12 +74,14 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please check later.</p>
       )}
 
       {/* <Pizza
@@ -122,11 +124,15 @@ function Footer() {
   // && -> (x && y) x false ise y ye bakılmayacak bile direkt x'i dönecek. ikisi de true ise y yi dönücek.
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00 :)
+        </p>
       )}
     </footer>
   );
