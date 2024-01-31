edc58f111f8c40172fa3402edd043716d4b859cb
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-01-31 09:22:28 +0300

    handle submit event

--- 05-travel-list/v2/src/App.js
+++ 05-travel-list/v2/src/App.js
@@ -19,8 +19,13 @@ function Logo() {
 }
 
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e); // SyntheticBaseEvent {_reactName: 'onSubmit', _targetInst: null, type: 'submit', nativeEvent: SubmitEvent, target: form.add-form, …}
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
       <select>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
