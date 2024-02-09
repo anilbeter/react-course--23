8b37e09264cfaed18ff6954da0a9931edff513fa
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-09 09:39:16 +0300

    use PropTypes

--- 07-usepopcorn/starter/src/StarRating.js
+++ 07-usepopcorn/starter/src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -8,6 +9,17 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
+};
+
+// If this is gonna really important (for real world project), use TypeScript instead of JavaScript(PropTypes)
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
 };
 
 export default function StarRating({
