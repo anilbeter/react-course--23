203dcd60e108def2ef67bbf705da0935f3d2aff6
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-18 09:29:37 +0300

    state update batching

--- 08-how-react-works/starter/src/App.js
+++ 08-how-react-works/starter/src/App.js
@@ -72,8 +72,33 @@ function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  console.log("RENDER");
+
   function handleInc() {
     setLikes(likes + 1);
+  }
+
+  function handleTripleInc() {
+    /*
+    setLikes(likes + 1);
+    setLikes(likes + 1);
+    setLikes(likes + 1);
+     -> likes 3 kere artmayacak çünkü re-render asynchronous. bu scope bitene kadar likes sürekli 0 kalmaya devam edecek en sonunda da 0 + 1 = 1, ekranda 3 artması gerekirken 1 artacak
+    */
+
+    //  Proper way:
+    setLikes((likes) => likes + 3);
+  }
+
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+    console.log(likes);
+  }
+
+  // React 18+ do have automating batching
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
   }
 
   return (
@@ -89,13 +114,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
