1db8518b419fae61984f1c01e38539d9d5bbb276
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-02-18 20:50:01 +0300

    wrong way to fetch data

--- 07-usepopcorn/starter/src/App.js
+++ 07-usepopcorn/starter/src/App.js
@@ -52,10 +52,12 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&`);
+  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=13th-friday`).then((res) =>
+    res.json().then((data) => console.log(data))
+  );
 
   return (
     <>
