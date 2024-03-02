7a1f2c6a73f079aac691bafb66018e8864b215d2
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-03-02 21:37:01 +0300

    add solution #1

--- 07-usepopcorn/starter/currency-converter/src/App.js
+++ 07-usepopcorn/starter/currency-converter/src/App.js
@@ -1,22 +1,59 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [fromCurrentCurrency, setFromCurrentCurrency] = useState("USD");
+  const [toCurrentCurrency, setToCurrentCurrency] = useState("EUR");
+  const [amount, setAmount] = useState(100);
+  const [convertedCurr, setConvertedCurr] = useState(0);
+
+  useEffect(
+    function () {
+      async function fetchCurrency() {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrentCurrency}&to=${toCurrentCurrency}`
+        );
+        const data = await res.json();
+        console.log(data);
+        return data.rates[toCurrentCurrency];
+      }
+      fetchCurrency().then((rate) => setConvertedCurr(rate));
+    },
+    [fromCurrentCurrency, toCurrentCurrency, amount]
+  );
+
+  // Yalnızca döviz kuru değiştiğinde güncellenir.
+  useEffect(() => {
+    console.log(convertedCurr);
+  }, [convertedCurr]);
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select
+        value={fromCurrentCurrency}
+        onChange={(e) => setFromCurrentCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={toCurrentCurrency}
+        onChange={(e) => setToCurrentCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>OUTPUT</p>
+      <p>{convertedCurr}</p>
     </div>
   );
 }
