5553b30269aa02edb0d27e62d61582aa5156a66c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-03-07 08:35:30 +0300

    useRef to select DOM element

--- 07-usepopcorn/starter/src/App.js
+++ 07-usepopcorn/starter/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import KEY from "./key.js";
 import StarRating from "./StarRating.js";
 
@@ -164,12 +164,18 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
   useEffect(function () {
-    // this is not REACT way
-    const el = document.querySelector(".search");
-    console.log(el);
-    el.focus();
+    console.log(inputEl.current);
+    inputEl.current.focus();
   }, []);
+
+  // useEffect(function () {
+  //   const el = document.querySelector(".search");
+  //   console.log(el);
+  //   el.focus();
+  // }, []);
 
   return (
     <input
@@ -178,6 +184,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
