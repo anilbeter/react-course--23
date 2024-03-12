276bf75eb96a7632dcd14ab15498d89331f871f9
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-03-12 06:58:36 +0300

    lifecycle components

--- 09-classy-weather/starter/src/App.js
+++ 09-classy-weather/starter/src/App.js
@@ -36,7 +36,7 @@ function formatDay(dateStr) {
 
 class App extends React.Component {
   state = {
-    location: 'Los Angeles',
+    location: '',
     isLoading: false,
     displayLocation: '',
     weather: {},
@@ -44,6 +44,8 @@ class App extends React.Component {
 
   // async fetchWeather() {
   fetchWeather = async () => {
+    if (this.state.location.length < 2) return this.setState({ weather: {} });
+
     try {
       this.setState({ isLoading: true });
 
@@ -69,13 +71,29 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
   setLocation = (e) => this.setState({ location: e.target.value });
+
+  // useEffect []
+  componentDidMount() {
+    // this.fetchWeather();
+
+    this.setState({ location: localStorage.getItem('location') || '' });
+  }
+
+  // useEffect [location]
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+
+      localStorage.setItem('location', this.state.location);
+    }
+  }
 
   render() {
     return (
@@ -85,7 +103,6 @@ class App extends React.Component {
           location={this.state.location}
           onChangeLocation={this.setLocation}
         />
-        <button onClick={this.fetchWeather}>Get Weather</button>
 
         {this.state.isLoading && <p className="Loader">Loading...</p>}
 
@@ -118,6 +135,10 @@ class Input extends React.Component {
 }
 
 class Weather extends React.Component {
+  componentWillUnmount() {
+    console.log('Weather will unmount');
+  }
+
   render() {
     const {
       temperature_2m_max: max,
