f70e10abd206d1afd21f89632947d61297720160
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-03-13 19:34:03 +0300

    load questions

--- 10-react-quiz/starter/src/App.js
+++ 10-react-quiz/starter/src/App.js
@@ -1,7 +1,43 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
 
-export default function app() {
+const initialState = {
+  questions: [],
+
+  //  "loading", "error", "ready", "active", "finished"
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch('http://localhost:9000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
