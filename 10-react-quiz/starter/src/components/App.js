b51d3151deebaa81021186d7590f446563a01424
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-03-14 08:00:21 +0300

    handle answering statement

--- 10-react-quiz/starter/src/components/App.js
+++ 10-react-quiz/starter/src/components/App.js
@@ -12,6 +12,7 @@ const initialState = {
   //  "loading", "error", "ready", "active", "finished"
   status: 'loading',
   index: 0,
+  answer: null,
 };
 
 function reducer(state, action) {
@@ -32,6 +33,11 @@ function reducer(state, action) {
         ...state,
         status: 'active',
       };
+    case 'newAnswer':
+      return {
+        ...state,
+        answer: action.payload,
+      };
 
     default:
       throw new Error('Action unknown');
@@ -39,7 +45,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -63,7 +69,13 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question question={questions[index]} />}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
