e933cb6e69652ce1e0aa152d0c43d645b3e7b4d0
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-03-17 10:07:48 +0300

    add Route elements

--- 11-worldwise/starter/worldwise/src/App.jsx
+++ 11-worldwise/starter/worldwise/src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import Homepage from './pages/Homepage';
+import PageNotFound from './pages/PageNotFound';
+
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        {/* if three routes that above won't match, the Route which have path="*" will be on the scene and will have called <PageNotFound /> component */}
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
