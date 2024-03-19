859972fbdc2c1f9bed55b968b1884dd3fdc1ee92
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2024-03-19 20:35:28 +0300

    read and set query string

--- 11-worldwise/starter/src/components/City.jsx
+++ 11-worldwise/starter/src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -10,6 +10,10 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
+  const [searchParams, setSearch] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   const { id } = useParams();
 
   // TEMP DATA
